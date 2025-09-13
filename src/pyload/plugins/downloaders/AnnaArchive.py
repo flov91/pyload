0c3f8d6fc285dfb6fab8d219a9ddabad05e75e39
Flo <florian@bany.me>
2025-09-13 22:25:24 +0200

    Update Anna_archive plugin

--- src/pyload/plugins/downloaders/AnnaArchive.py
+++ src/pyload/plugins/downloaders/AnnaArchive.py
@@ -2,6 +2,8 @@
 import re
 import requests
 
+from pyload.core.utils import parse
+
 from ..base.simple_downloader import SimpleDownloader
 from ..helpers import search_pattern
 
@@ -18,10 +20,12 @@ class AnnaArchive(SimpleDownloader):
         ("enabled", "enabled", "Activated", True),
         ("use_premium", "bool", "Use premium account if available", False),
         ("fallback", "bool", "Fallback to free download if premium fails", True),
+        ("chunk_limit", "int", "Default number of chunk", 1),
+        ("resume_download", "bool", "Option to resume download in case of issue", True),
         ("flaresolverr_url", "string", "flaresolverr url for cloudflare bypass", "http://127.0.0.1:8191/v1"),
         ("flaresolverr_max_timeout", "int", "Max timeout for cloudflare challenge (in seconde)", 60),
         ("extension_regexp", "string", "", "pdf|epub|zip|mobi|fb2|cbr|djvu|cbz|doc|rtf|rar|docx|tar|7z|gz|cb7")
-        
+
     ]
 
     __description__ = """Plugin for AnnaArchive / Welib (need flaresolverr)"""
@@ -58,13 +62,15 @@ class AnnaArchive(SimpleDownloader):
 
     OFFLINE_PATTERN = r'File Not Found'
     LINK_FREE_PATTERN = r"(/slow_download/.*?)['|\"]"
+    URL_FREE_PATTERN = r"(/slow_download/)"
     LINK_PREMIUM_PATTERN = r"(/fast_download/.*?)['|\"]"
+    DL_LIMIT_PATTERN = r' slow down '
 
     def setup(self):
-        self.chunk_limit = 1
-        self.resume_download = False
         self.multi_dl = False
         self.FINAL_LINK_PATTERN = rf"(https://.*?\.(?:{self.config.get("extension_regexp")}))['|\"]"
+        self.chunk_limit = self.config.get("chunk_limit") # 1
+        self.resume_download = self.config.get("resume_download") # True
         self.flaresolverr_url = self.config.get("flaresolverr_url")
         self.flaresolverr_max_timeout = self.config.get("flaresolverr_max_timeout")
 
@@ -89,24 +95,47 @@ class AnnaArchive(SimpleDownloader):
                 self.log_error("FlareSolverr request failed: {}".format(e))
                 return None
 
+    def handle_direct(self, pyfile):
+        print(self.link)
+        print(pyfile.url)
+        print('#############################################################################"')
+        if search_pattern(self.URL_FREE_PATTERN, pyfile.url):
+            cloud_url = self.flaresolverr_request(pyfile.url, 70)
+            if cloud_url:
+                m = re.search(self.FINAL_LINK_PATTERN, cloud_url.replace('\\n', '').replace('\\"', '"'))
+                if m:
+                    link = self.isresource(m.group(1))
+                    if link:
+                        pyfile.name = parse.name(link)
+                        self.link = m.group(1)
+                    else:
+                        self.link = None
+                else:
+                    self.link = None
+                #     self.error(self._("Free download link not found on final page"))
+            else:
+               self.error(self._("Issue with flaresollverr")) 
+        else:
+            self.link = None
+
     def handle_free(self, pyfile):
         if not self.LINK_FREE_PATTERN:
             self.fail(self._("Free download not implemented"))
 
         m = search_pattern(self.LINK_FREE_PATTERN, self.data)
-        if m is None:
-            self.error(self._("Free download link not found"))
-        else:
+        if m:            
             self.base_url = re.search(r"^(https?://[^/]+)", pyfile.url).group(1)
             slow_url = self.base_url + m.group(1)
             self.log_info(f"Free download inter link found '{slow_url}' going to flaresolverr")
             slow_page = self.flaresolverr_request(slow_url, 70).replace('\\n', '').replace('\\"', '"')
             m = re.search(self.FINAL_LINK_PATTERN, slow_page)
-            if m is None:
-                self.error(self._("Free download link not found on final page"))
-            else:
-              self.link = m.group(1)  
-        
+            if m:
+                self.link = m.group(1)
+            # else:
+            #     self.error(self._("Free download link not found on final page"))
+        # else:
+        #     self.error(self._("Free download link not found"))
+
 
     def handle_premium(self, pyfile):
         self.log_warning(self._("Premium download not implemented yet"))
